2cef25632cd02434666fb07351beb33cf853608e
Justin Hui <jhui@lcs.on.ca>
2023-04-05 11:52:27 -0400

    Initial Commit

---
+++ MoodMapper/MoodMapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMapperApp.swift
+//  MoodMapper
+//
+//  Created by Justin Hui on 5/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoodMapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
